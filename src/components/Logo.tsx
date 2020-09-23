8ef0e24ef324e3273cfd12c27198099cfa34a070
Jones Ogolo <jay.topher11@gmail.com>
2020-09-23 16:04:40 +0100

    created logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logo from '../assets/logo.svg';
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt="Easy bank" />
+    </div>
+  );
+};
+
+export default Logo;
