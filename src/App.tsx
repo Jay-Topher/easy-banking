59c09f208a4e77d72d7765874586f9531a160406
Jones Ogolo <jay.topher11@gmail.com>
2020-09-24 22:25:04 +0100

    mounted footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import AdvertsSection from "./components/AdvertsSection";
+import FooterSection from "./components/FooterSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import SecondSection from "./components/SecondSection";
@@ -12,6 +13,7 @@ function App() {
       <HeroSection />
       <SecondSection />
       <AdvertsSection />
+      <FooterSection />
     </div>
   );
 }
