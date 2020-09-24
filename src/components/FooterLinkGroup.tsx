39fa279815bbcdff18773e471c4cee85ec46588f
Jones Ogolo <jay.topher11@gmail.com>
2020-09-24 22:24:16 +0100

    created a link group component

---
+++ src/components/FooterLinkGroup.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { myTheme as theme } from "../my-theme";
+
+interface FooterLinkGroupProps {
+  links: {
+    name: string;
+    link: string;
+  }[];
+}
+
+const FooterLinkGroup: React.FC<FooterLinkGroupProps> = ({ links }) => {
+  return (
+    <LinkGroup>
+      {links.map(({ name, link }) => (
+        <a href={link} key={name}>
+          {name}
+        </a>
+      ))}
+    </LinkGroup>
+  );
+};
+
+export default FooterLinkGroup;
+
+const LinkGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+
+  @media (max-width: ${theme.breakpoints.lg}) {
+    align-items: flex-start;
+    margin-left: 8rem;
+  }
+
+  a {
+    margin-bottom: 1rem;
+    text-decoration: none;
+    color: ${theme.colors.neutral.white};
+    font-size: .9rem;
+  }
+`;
