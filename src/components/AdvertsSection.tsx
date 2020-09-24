4a27f741c8d3726a1885d6606cf89977a0eaf8c9
Jones Ogolo <jay.topher11@gmail.com>
2020-09-24 10:26:17 +0100

    created advert layout

--- src/components/AdvertsSection.tsx
+++ src/components/AdvertsSection.tsx
@@ -1,20 +1,58 @@
-import React from 'react'
-import Container from './Container'
+import React from "react";
+import styled from "styled-components";
+import { myTheme } from "../my-theme";
+import { adverts } from "../assets/data.json";
+import Container from "./Container";
+import AdvertItem from "./AdvertItem";
+import imageCurrency from "../assets/image-currency.jpg";
+import imageRestaurant from "../assets/image-restaurant.jpg";
+import imageConfetti from "../assets/image-confetti.jpg";
+import imagePlane from "../assets/image-plane.jpg";
+import FlexedContainer from "./FlexedContainer";
 
-interface AdvertsSectionProps {
+interface AdvertsSectionProps {}
 
-}
-
-const AdvertsSection: React.FC<AdvertsSectionProps> = ({}) => {
-    return (
-      <div>
-        <Container>
-          <div>
-            <h2>Latest Articles</h2>
-          </div>
-        </Container>
-      </div>
-    );
-}
+const AdvertsSection: React.FC<AdvertsSectionProps> = () => {
+  const images = [imageCurrency, imageRestaurant, imagePlane, imageConfetti];
+  return (
+    <AdvertContainer>
+      <Container>
+        <AdvertsMain>
+          <h2>Latest Articles</h2>
+          <Flexed>
+            {adverts.map(({ author, title, description }, index) => (
+              <AdvertItem
+                author={author}
+                title={title}
+                description={description}
+                photo={images[index]}
+              />
+            ))}
+          </Flexed>
+        </AdvertsMain>
+      </Container>
+    </AdvertContainer>
+  );
+};
 
 export default AdvertsSection;
+
+const AdvertContainer = styled.section`
+  background-color: ${myTheme.colors.neutral.veryLightGray};
+`;
+
+const AdvertsMain = styled.section`
+  height: inherit;
+  padding: 8rem 0;
+
+  h2 {
+    font-weight: 400;
+    font-size: 2.5rem;
+    margin-bottom: 1.9rem;
+  }
+`;
+
+const Flexed = styled(FlexedContainer)`
+  align-items: flex-start;
+  justify-content: space-between;
+`;
