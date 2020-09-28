6d8f4075e99ba325d5a125e54c8b4733db499b84
Jones Ogolo <jay.topher11@gmail.com>
2020-09-28 09:57:14 +0100

    navbar is responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import Container from "./Container";
 import { navLinks } from "../assets/data.json";
 import InviteButton from "./InviteButton";
 import { myTheme as theme } from "../my-theme";
+import Hamburger from "./Hamburger";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleShow = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setOpen(!open);
+  };
   return (
     <Nav>
       <Container>
@@ -21,6 +27,10 @@ const Navbar = () => {
           </UL>
           <InviteButton>Register Here</InviteButton>
         </Div>
+        <MobileDiv>
+          <Logo />
+          <Hamburger open={open} onClick={toggleShow} />
+        </MobileDiv>
       </Container>
     </Nav>
   );
@@ -28,6 +38,9 @@ const Navbar = () => {
 
 const UL = styled.ul`
   list-style: none;
+
+  @media (max-width: ${theme.breakpoints.lg}) {
+  }
 
   & > li {
     display: inline;
@@ -46,10 +59,14 @@ const UL = styled.ul`
 `;
 
 const Div = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  display: none;
   padding: 15px 0;
+
+  @media only screen and (min-width: ${theme.breakpoints.md}) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const Nav = styled.nav`
@@ -58,6 +75,16 @@ const Nav = styled.nav`
   position: sticky;
   top: 0;
   font-size: 1rem;
-`
+`;
+
+const MobileDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.2rem 1rem;
+
+  @media (min-width: ${theme.breakpoints.md}) {
+    display: none;
+  }
+`;
 
 export default Navbar;
